b1c2cf4e56362123f9f24db7454b8b2cdf1ead89
Kriba95 <kribadiba@gmail.com>
2021-05-02 23:36:26 +0300

    commit main

---
+++ index.php
@@ -0,0 +1,79 @@
+
+
+
+<!DOCTYPE html>
+<html>
+<!--2021 Kristian Töllikkö, Lämpötilanmuunnin 0.1-->
+<head>
+<title>Kaverilista</title>
+<link rel="stylesheet" href="css/main.css"> 
+<link rel="stylesheet" href="css/animate.css">
+
+
+<meta charset="utf-8">
+
+</head>
+
+<body>
+    <br>
+    <br>
+    <br>
+
+<section Lämpötilanmuunnin>
+  <div class="container wow pulse">
+    <div class="teksti">
+      <p class="kav-teksti">Lämpötilanmuunnin</p>
+      <hr style="">
+      <div class="js-boksi">
+      <select id="lampotila">
+        <option value="celcius">Celcius</option>
+        <option value="fahrenheit">Fahrenheit</option>
+      </select>
+
+      <div>
+        <p>
+          <input type="radio" id="yddesi"
+          name="contact" value="yddesi" checked> 
+          <label for="contactChoice1">1desi</label>
+        </p>
+        <p>
+          <input type="radio" id="kadesi"
+          name="contact" value="kadesi">
+          <label for="contactChoice2">2desi</label>
+        </p>
+        <p>
+          <input type="radio" id="kodesi"
+          name="contact" value="kodesi">
+          <label for="contactChoice3">3desi</label>
+        </p>
+      </div>
+  
+        <input style="width: 70%" type="text" name="nimi" id="arvo" placeholder="Luku, Kiitos!">
+        <button type="button" onclick="tempConvert()" class="Btn">Muunna</button>
+        </form> 
+      </div> 
+      <br>
+      <div class="grid-boksi"><h1>
+        <p style="display: inline-block;" id="output">Vastaus </p><p id="kumpi" style="display: inline-block;" ></p>
+        </h1></div>
+    </div>
+  </div>    
+</section> 
+      
+
+
+</body>
+
+<script src="js/lampotilamuunnin.js"></script>
+
+<!-- Koska työ on julkinen, latasin nopeasti "WOW" animaation-->
+<script src="js/selectbox.js"></script>
+<script src="js/wow.min.js"></script>
+<script> new WOW().init(); 
+// Jälkimmäinen päivättää sivun == 2 on arvo selaimessa joka tunnistaa palaavan käyttäjän
+if(performance.navigation.type == 2){ location.reload(true);}</script>
+
+
+</html>
+
+
---
+++ js/lampotilamuunnin.js
@@ -0,0 +1,45 @@
+// document.getElementById('Muunna').onclick = tempConvert;
+
+
+function tempConvert() {
+
+    const val = document.getElementById("lampotila").value;
+    const yd = document.querySelector('#yddesi');
+    const ka = document.querySelector('#kadesi');
+    const ko = document.querySelector('#kodesi');
+    let lampoArvo = document.querySelector('#arvo').value;
+   
+
+
+    
+    
+    if (val === "celcius"  && yd.checked==true) {
+        document.getElementById("output").innerHTML = (parseFloat(lampoArvo) - 32) * 0.5556.toFixed(2);
+        document.getElementById("kumpi").innerHTML = "°C";
+
+
+
+    } else if (val === "celcius" && ka.checked==true) {
+        document.getElementById("output").innerHTML = (parseFloat(lampoArvo) - 32) * 0.5556.toFixed(3);
+        document.getElementById("kumpi").innerHTML = "°C";
+      }
+      else if (val === "celcius" && ko.checked==true) {
+
+        document.getElementById("output").innerHTML = (parseFloat(lampoArvo) - 32) * 0.5556.toFixed(4);
+        document.getElementById("kumpi").innerHTML = "°C";
+      }
+      else if (val === "fahrenheit" && yd.checked==true) {
+        document.getElementById("output").innerHTML = ((parseFloat(lampoArvo) * 1.8) + 32).toFixed(2);
+        document.getElementById("kumpi").innerHTML = "°F";
+      }
+      else if (val === "fahrenheit" && ka.checked==true) {
+
+        document.getElementById("output").innerHTML = ((parseFloat(lampoArvo) * 1.8) + 32).toFixed(3);
+        document.getElementById("kumpi").innerHTML = "°F";
+      }
+      else if (val === "fahrenheit" && ko.checked==true) {
+        document.getElementById("output").innerHTML = ((parseFloat(lampoArvo) * 1.8) + 32).toFixed(4);
+        document.getElementById("kumpi").innerHTML = "°F";
+      }
+      
+    }
